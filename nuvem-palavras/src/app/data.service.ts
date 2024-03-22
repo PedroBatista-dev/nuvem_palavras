0e34abd009253d4204ae19fe5afb1633fdf59f7c
Pedro Batista <phmbbatista@gmail.com>
2024-03-21 21:25:09 -0300

    codando a tela de palavras

--- nuvem-palavras/src/app/data.service.ts
+++ nuvem-palavras/src/app/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, timer } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,15 @@ export class DataService {
 
   enviarResposta(resposta: string): Observable<any> {
     return this.http.post<any>('URL_DA_SUA_API', { resposta });
+  }
+
+  getMensagem(): Observable<string> {
+    return timer(0, 1000).pipe(
+      map(() => {
+        // Simulando uma resposta de servidor
+        const randomNumber = Math.floor(Math.random() * 1000000);
+        return `Mensagem ${randomNumber}`;
+      })
+    );
   }
 }
--- nuvem-palavras/src/app/tela-palavras/tela-palavras.component.ts
+++ nuvem-palavras/src/app/tela-palavras/tela-palavras.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-tela-palavras',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./tela-palavras.component.css']
 })
 export class TelaPalavrasComponent {
+  mensagens: string[] = [];
 
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.getMensagens();
+  }
+
+  getMensagens(): void {
+    this.dataService.getMensagem().subscribe(frase => {
+      this.mensagens.unshift(frase); // Adicionando a nova mensagem no início do array
+    });
+  }
+
+  getTamanhoClasse(mensagem: string): string {
+    return mensagem.length < 13 ? 'small' : 'large'; // Define a classe com base no tamanho da mensagem
+  }
 }
